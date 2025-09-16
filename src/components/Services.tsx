4a2dc49708fc79048cf547f1e3589dbd187a2777
assessoriajhessiimoura-gif <assessoriajhessiimoura@gmail.com>
2025-09-16 11:59:14 -0300

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -8,7 +8,7 @@ const Services = () => {
       caption: 'Mega Volume'
     },
      {
-      src: '/images/WhatsApp Image 2025-09-13 at 18.55.50.jpeg',
+      src: '/images/WhatsApp Image 2025-09-13 at 18.55.51.jpeg',
       alt: 'Resultado de extensão de cílios - Volume perfeito',
       caption: '(Volume Brasileiro)'
     },
